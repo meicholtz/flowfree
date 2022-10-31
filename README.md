ea36f53525bc1dd608ee67d9c88859ce934ad4e9
Eicholtz <meicholtz@flsouthern.edu>
2022-10-30 23:05:07 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Flow Free
+
+# My initial attempt to emulate the game [flow free](https://en.wikipedia.org/wiki/Flow_Free) in Python.
---
+++ flowfree.py
@@ -0,0 +1,11 @@
+# flowfree.py
+# Play Flow Free in Python!
+
+import argparse
+from graphics import *
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
